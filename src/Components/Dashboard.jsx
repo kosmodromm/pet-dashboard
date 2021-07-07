3d9a0b1183c94803c5b92e7b7abc232927f2479f
Anton Kizerev <70804175+kosmodromm@users.noreply.github.com>
2021-07-07 14:36:58 +0300

    add: simple structures

---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,15 @@
+export default function Dashboard () {
+    return (
+        <div className="App-body">
+            <div className="crypto-app">
+                Crypto
+            </div>
+            <div className="planner-app">
+                Planner
+            </div>
+            <div className="weather-app">
+                Weather
+            </div>
+        </div>
+    )
+}
---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header () {
+    return (
+        <div className="App-header">
+            <div>
+                Apps
+            </div>
+            <div>
+                Contacts
+            </div>
+            <div>
+                Login
+            </div>
+        </div>
+    )
+}
